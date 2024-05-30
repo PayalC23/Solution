c11f15514a5b1ee6820911f2e0f35217046a809a
payal <payal@LAPTOP-MLLCE3TH>
2024-05-30 22:23:05 +0530

    First Commit

---
+++ src/Solution/Mult.java
@@ -0,0 +1,13 @@
+package Solution;
+
+public class Mult {
+
+	public static void main(String[] args) {
+		int a= 2, b=3;
+		int c;
+		c = a*b;
+		System.out.println("Multiplication is :"+c);
+
+	}
+
+}
